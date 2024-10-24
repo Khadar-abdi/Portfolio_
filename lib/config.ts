ec560643a96b5fe42b2e46308e7960ca90977358
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2024-10-24 17:00:02 +0300

    update config file

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NODE_ENV=== 'production'? 'portfoliokhader.vercel.app'
-: 'http://localhost:3000';
+export const API_URL = process.env.NODE_ENV=== 'production' && 'portfoliokhader.vercel.app';
+ 
